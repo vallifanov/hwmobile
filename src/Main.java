38a78314b17380272872dcfae19a3e116797bc1f
vallifanov <vallifanov@gmail.com>
2022-03-30 17:46:43 +1200

    Форматирование кода

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ public class Main {
     public static void main(String[] args) {
 //        Входные данные
         int balance = 100;
-        int refill = 1100;
+        int refill = 900;
         int part = 100;
 //        Логика программы
         int bonus;
-        if (refill > 1_000) {
+        if (refill > 1000) {
             bonus = refill / part;
-        } else { bonus = 0;}
+        } else {
+            bonus = 0;
+        }
         int total = balance + refill + bonus;
         System.out.println(total);
         System.out.println(bonus);
